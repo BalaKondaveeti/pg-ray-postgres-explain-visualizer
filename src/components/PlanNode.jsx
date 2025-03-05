02b88a980bd307a073f9ad61132000a68ed625f5
BalaKondaveeti <bkondave@asu.edu>
2025-03-05 15:42:12 -0800

    Complete skeleton

---
+++ src/components/PlanNode.jsx
@@ -0,0 +1,7 @@
+// src/components/PlanNode.jsx
+
+const getNodeStyle = () => {};
+
+const getIcon = () => {};
+
+export default memo(({ }) => {});
---
+++ src/utils/parser.js
@@ -0,0 +1,8 @@
+// src/utils/parser.js
+
+/**
+ * Recursively parses the Postgres JSON plan into Nodes and Edges
+ */
+export const parsePlanToGraph = () => {};
+
+export const layoutElements = () => {};
